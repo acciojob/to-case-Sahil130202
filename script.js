0e7fce8ba2ae541fa3a9b6c5db0913994829bae2
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-04 15:08:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function toCase(text) {
   // write your code here
-	return text.toLowerCase() + "-" + text.toUpperCase()
+	if(text===null)
+	{
+		return []
+	}	return text.toLowerCase() + "-" + text.toUpperCase()
 	
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
-alert(toCase(text));
+/alert(toCase(text));
